3fb9381b6c5ad48be0ce1d981997af9eaa07bcd1
SebbyBlob0 <43016543+SebbyBlob0@users.noreply.github.com>
2020-08-14 02:14:27 -0400

    commit

--- src/main/java/com/blob/discord/Core.java
+++ src/main/java/com/blob/discord/Core.java
@@ -14,8 +14,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Core {
 
